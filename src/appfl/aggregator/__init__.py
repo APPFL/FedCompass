bf1862f90ca830d9e5b36a72e682deec0c5647d8
Zilinghan <zl52@illinois.edu>
2024-04-01 18:06:06 -0500

    Add FedAdam

--- src/appfl/aggregator/__init__.py
+++ src/appfl/aggregator/__init__.py
@@ -1,5 +1,6 @@
 from .base_aggregator import BaseAggregator
 from .fedavg_aggregator import FedAvgAggregator
+from .fedadam_aggregator import FedAdamAggregator
 from .fedadagrad_aggregator import FedAdagradAggregator
 from .fedasync_aggregator import FedAsyncAggregator
 from .fedcompass_aggregator import FedCompassAggregator
